819680a768e4a5618d0aa36aae4f0d8380e76d28
Shaurya Tripathi <88775162+shauryatripaathi@users.noreply.github.com>
2021-11-29 23:08:43 +0530

    Update init.js

--- init.js
+++ init.js
@@ -1,22 +1,32 @@
-let container = document.getElementById("container");
-let boxAppear = document.createElement("div");
-boxAppear.id = "boxAppear";
-boxAppear.innerHTML = "Hello Welcome!!";
-boxAppear.style.display = "none";
-container.append(boxAppear);
-let btn = document.getElementById("btn");
-btn.addEventListener("click" , clickBtn);
+let mainContainer = document.getElementById("container");
+let btn = document.createElement("button");
+btn.id ="btn";
+btn.innerHTML = "click me";
+mainContainer.append(btn);
+let popUp = document.createElement("div");
+popUp.id = "popUp";
+popUp.innerHTML = Date();
+popUp.style.display = "none";
+mainContainer.append(popUp);
+let innerBox = document.createElement("div");
+innerBox.id = "innerBox";
+popUp.append(innerBox);
+innerBox.innerHTML = "Hello!! how are you"
+btn.addEventListener("click",clickBtn);
 function clickBtn(){
-    boxAppear.style.display = "block";
-    container.style.backgroundColor="black";
+    popUp.style.display = "block";
+    btn.style.display = "none";
 }
-container.addEventListener("click",boxDisappear);
-function boxDisappear(){
-    boxAppear.style.display = "none";
-}
+
+
+
+
 
 
 // create box before but display after click , ok
 // body turns black after box appears, ok
 // box disappears after click anywhere on the screen, ok
-// cross icon on the box
+// cross icon on the box------
+// naming
+// refactor - when u make changes in code 
+// questions -can we give same name to class id --
