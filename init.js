4af1ced57c4afe62c59dc25aa74c536612f6dda0
Shaurya Tripathi <88775162+shauryatripaathi@users.noreply.github.com>
2021-11-30 18:44:58 +0530

    Update init.js

--- init.js
+++ init.js
@@ -1,23 +1,39 @@
 let mainContainer = document.getElementById("container");
 let btn = document.createElement("button");
-btn.id ="btn";
+btn.id = "btn";
 btn.innerHTML = "click me";
 mainContainer.append(btn);
+
 let popUp = document.createElement("div");
 popUp.id = "popUp";
-popUp.innerHTML = Date();
 popUp.style.display = "none";
 mainContainer.append(popUp);
+
+let boxShowingDate = document.createElement("div");
+boxShowingDate.id = "boxShowingDate";
+boxShowingDate.innerHTML = Date();
+popUp.append(boxShowingDate);
+
 let innerBox = document.createElement("div");
 innerBox.id = "innerBox";
 popUp.append(innerBox);
 innerBox.innerHTML = "Hello!! how are you"
-btn.addEventListener("click",clickBtn);
-function clickBtn(){
+
+btn.addEventListener("click", clickBtn);
+function clickBtn() {
     popUp.style.display = "block";
     btn.style.display = "none";
 }
 
+let crossBtn = document.createElement("button");
+crossBtn.id = "crossBtn";
+crossBtn.innerHTML = "X"
+popUp.append(crossBtn);
+crossBtn.addEventListener("click", handleOnCrossBtn);
+function handleOnCrossBtn(){
+    popUp.style.display = "none";
+    btn.style.display = "block";
+}
 
 
 
@@ -26,7 +42,6 @@ function clickBtn(){
 // create box before but display after click , ok
 // body turns black after box appears, ok
 // box disappears after click anywhere on the screen, ok
-// cross icon on the box------
+// cross icon on the box ok
 // naming
 // refactor - when u make changes in code 
-// questions -can we give same name to class id --
